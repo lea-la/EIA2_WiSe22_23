1fc4a611d4108d551062cc0edbac48af74afa6d5
lea-la <lea.lange@hs-furtwangen.de>
2022-11-26 22:07:00 +0100

    L08 upload

--- L08.1_GenerativeArt/GenerativeArt.js
+++ L08.1_GenerativeArt/GenerativeArt.js
@@ -17,11 +17,11 @@ var L08_GenerativeArt;
         drawCloud({ x: 180, y: 130 }, { x: 100, y: 50 });
         drawCloud2({ x: 550, y: 190 }, { x: 90, y: 50 });
         drawCloud2({ x: 710, y: 190 }, { x: 110, y: 90 });
-        drawMountains({ x: 0, y: crc2.canvas.height - 50 }, 120, 150);
-        drawMountains({ x: 0, y: crc2.canvas.height }, 130, 150);
+        drawWaves({ x: 0, y: crc2.canvas.height - 50 }, 120, 150);
+        drawWaves({ x: 0, y: crc2.canvas.height }, 130, 150);
+        drawSand({ x: 0, y: 460 }, 70, 90);
     }
     function drawBackground() {
-        console.log("Background");
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
         gradient.addColorStop(0.3, "#7FA0B8");
         gradient.addColorStop(golden, "#FFC9BD");
@@ -30,7 +30,6 @@ var L08_GenerativeArt;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
     function drawSun(_position) {
-        console.log("Sun", _position);
         let r1 = 30;
         let r2 = 120;
         let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
@@ -45,7 +44,6 @@ var L08_GenerativeArt;
         crc2.restore();
     }
     function drawCloud(_position, _size) {
-        console.log("Cloud", _position, _size);
         let nParticles = 40;
         let radiusParticle = 30;
         let particle = new Path2D;
@@ -88,21 +86,21 @@ var L08_GenerativeArt;
         }
         crc2.restore();
     }
-    function drawStreet(_position, _widthBack, _widthFront) {
+    function drawSand(_position, _widthBack, _widthFront) {
         crc2.beginPath();
-        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
-        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
-        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
-        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+        crc2.moveTo(_position.x, _position.y);
+        crc2.lineTo(_position.x, _position.y + 100);
+        crc2.lineTo(_position.x + 400, _position.y + 100);
         crc2.closePath();
         let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "darkgrey");
-        gradient.addColorStop(0.6, "black");
+        gradient.addColorStop(0, `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(230 * Math.random())}, ${Math.floor(180 * Math.random())})`);
+        gradient.addColorStop(0.8, `rgb(${Math.floor(230 * Math.random())}, ${Math.floor(200 * Math.random())}, ${Math.floor(210 * Math.random())})`);
         crc2.fillStyle = gradient;
         crc2.fill();
+        crc2.strokeStyle = "#FEE2B8";
+        crc2.stroke();
     }
-    function drawMountains(_position, _min, _max) {
-        console.log("Mountains");
+    function drawWaves(_position, _min, _max) {
         let stepMin = 10;
         let stepMax = 150;
         let x = 0;
@@ -114,14 +112,13 @@ var L08_GenerativeArt;
         do {
             x += stepMin + Math.random() * (stepMax - stepMin);
             let y = -_min - Math.random() * (_max - _min);
-            crc2.lineTo(x, y);
+            crc2.bezierCurveTo(x + 60, y + 60, x + 10, y + 10, x, y);
         } while (x < crc2.canvas.width);
         crc2.lineTo(x, 0);
         crc2.closePath();
-        //i, cnvs.height/2-Math.sin(i*(Math.PI/180))*50)
         let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
-        gradient.addColorStop(0, "hsla(192.12, 100%, 38.82%, 0.3)");
-        gradient.addColorStop(1, "hsla(186.27, 100%, 86.86%, 0.3)");
+        gradient.addColorStop(0, "hsla(192.12, 100%, 38.82%, 1)");
+        gradient.addColorStop(1, "hsla(186.27, 100%, 86.86%, 1)");
         crc2.fillStyle = gradient;
         crc2.fill();
         crc2.restore();
--- L08.1_GenerativeArt/GenerativeArt.ts
+++ L08.1_GenerativeArt/GenerativeArt.ts
@@ -23,13 +23,15 @@ namespace L08_GenerativeArt {
      drawCloud({x: 180, y: 130}, {x: 100, y: 50});
      drawCloud2({x: 550, y: 190}, {x: 90, y: 50});
      drawCloud2({x: 710, y: 190}, {x: 110, y: 90});
-     drawMountains({x: 0 , y: crc2.canvas.height - 50}, 120, 150);
-     drawMountains({x: 0 , y: crc2.canvas.height}, 130 , 150);
+     drawWaves({x: 0 , y: crc2.canvas.height - 50}, 120, 150);
+     drawWaves({x: 0 , y: crc2.canvas.height}, 130 , 150);
+     drawSand ({x: 0, y: 460}, 70, 90 );
+    
+
   
     }
  
     function drawBackground(): void {
-     console.log("Background");
  
      let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
      gradient.addColorStop(0.3, "#7FA0B8");
@@ -45,7 +47,6 @@ namespace L08_GenerativeArt {
         
     
     function drawSun(_position: Vector): void {
-     console.log("Sun", _position);
      
      let r1: number = 30;
      let r2: number = 120;
@@ -67,7 +68,6 @@ namespace L08_GenerativeArt {
  
     
     function drawCloud(_position: Vector, _size: Vector): void {
-     console.log("Cloud", _position, _size);
  
      let nParticles: number = 40;
      let radiusParticle: number = 30;
@@ -123,24 +123,28 @@ namespace L08_GenerativeArt {
         
        }
  
-    function drawStreet(_position: Vector, _widthBack: number, _widthFront: number): void {
+    function drawSand(_position: Vector, _widthBack: number, _widthFront: number): void {
      crc2.beginPath();
-     crc2.moveTo(_position.x + _widthBack / 2, _position.y);
-     crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
-     crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
-     crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+     crc2.moveTo(_position.x , _position.y);
+     crc2.lineTo(_position.x, _position.y + 100);
+     crc2.lineTo(_position.x + 400 , _position.y + 100);
      crc2.closePath();
  
      let gradient: CanvasGradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
-     gradient.addColorStop(0, "darkgrey");
-     gradient.addColorStop(0.6, "black"); 
- 
+     gradient.addColorStop(0, `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(230 * Math.random())}, ${Math.floor(180 * Math.random())})`);
+     gradient.addColorStop(0.8, `rgb(${Math.floor(230 * Math.random())}, ${Math.floor(200 * Math.random())}, ${Math.floor(210 * Math.random())})`); 
+
+    
+      
      crc2.fillStyle = gradient;
      crc2.fill();
+
+     crc2.strokeStyle = "#FEE2B8";
+     crc2.stroke();
     }
     
-    function drawMountains(_position: Vector, _min: number, _max: number): void {
-    console.log("Mountains");
+    function drawWaves(_position: Vector, _min: number, _max: number): void {
+   
     let stepMin: number = 10;
     let stepMax: number = 150;
     let x: number = 0;
@@ -157,20 +161,20 @@ namespace L08_GenerativeArt {
          x += stepMin + Math.random () * (stepMax - stepMin);
          let y: number =  - _min - Math.random () * (_max - _min);
  
-         crc2.lineTo(x, y);
+         crc2.bezierCurveTo(x + 60, y + 60, x + 10, y + 10, x, y );
     } while (x < crc2.canvas.width);
  
     crc2.lineTo(x , 0);
     crc2.closePath();
-
-    //i, cnvs.height/2-Math.sin(i*(Math.PI/180))*50)
+   
  
     let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
-    gradient.addColorStop(0, "hsla(192.12, 100%, 38.82%, 0.3)");
-    gradient.addColorStop(1, "hsla(186.27, 100%, 86.86%, 0.3)");
+    gradient.addColorStop(0, "hsla(192.12, 100%, 38.82%, 1)");
+    gradient.addColorStop(1, "hsla(186.27, 100%, 86.86%, 1)");
  
     crc2.fillStyle = gradient;
     crc2.fill();
+    
  
     crc2.restore();
  
